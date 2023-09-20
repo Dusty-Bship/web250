da4eef3e1c5ab07c4a4afc8e85dd6e7c944be670
Dusty-Bship <dustyjblankenship@students.abtech.edu>
2023-09-20 12:01:34 -0400

    Finished bird.php

---
+++ asgn03-static/Bird.php
@@ -0,0 +1,36 @@
+<?php
+
+class Bird {
+    public $habitat;
+    public $food;
+    public $nesting = "tree";
+    public $conservation;
+    public $song = "chirp";
+    public $flying = "yes";
+    public static $instance_count = 0;
+    public static $egg_num = 0;
+
+    public function can_fly() {
+      ($this->flying == "yes" ) ? $flying_string = "can fly" : $flying_string = "is stuck on the ground";
+      return  $flying_string ;
+    }
+
+    public static function create() {
+      self::$instance_count++;
+      $newClass = get_called_class();
+      return new $newClass;
+    }
+}
+
+class YellowBelliedFlyCatcher extends Bird {
+    public $name = "yellow-bellied flycatcher";
+    public $diet = "mostly insects.";
+    public $song = "flat chilk";
+    public static $egg_num = "3-4, sometimes 5.";
+}
+
+class Kiwi extends Bird {
+    var $name = "kiwi";
+    var $diet = "omnivorous";
+    var $flying = "no";
+}
