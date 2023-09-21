fb6b9142e6574014aa386c8b2ed43662e1ccbd33
Dusty-Bship <dustyjblankenship@students.abtech.edu>
2023-09-20 23:18:35 -0400

    completed bird/index.php

--- asgn03-static/index.php
+++ asgn03-static/index.php
@@ -6,7 +6,7 @@
     <title>asgn02 Inheritance</title>
 </head>
 <body>
-<h1>Inheritance Examples</h1>
+<h1>Inheritance Examples Before create()</h1>
 
 <?php 
     include 'Bird.php';
@@ -21,8 +21,25 @@
     $kiwi->flying = "no";
     echo "<p>The " . $fly_catcher->name . " " . $fly_catcher->can_fly() . ".</p>";
     echo "<p>The " . $kiwi->name . " " . $kiwi->can_fly() . ".</p>";    
-
+    
+    echo 'There is currently ' . Bird::$instance_count . ' instances of birds</br>';
+    echo 'There is currently ' . YellowBelliedFlyCatcher::$instance_count . ' instances of fly catcher</br>';
+    echo 'There is currently ' . Kiwi::$instance_count . ' instances of kiwi</br>';
+    
 ?>
-    </body>
+
+<h1>Inheritance Examples Using create()</h1>
+
+<?php
+    Bird::create();
+    YellowBelliedFlyCatcher::create();
+    Kiwi::create();
+
+    echo 'There is currently ' . Bird::$instance_count . ' instances of birds</br>';
+    echo 'There is currently ' . YellowBelliedFlyCatcher::$instance_count . ' instances of fly catcher</br>';
+    echo 'There is currently ' . Kiwi::$instance_count . ' instances of kiwi</br>';
+?>
+
+</body>
 </html>
 
