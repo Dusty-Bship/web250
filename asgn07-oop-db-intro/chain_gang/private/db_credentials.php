eab5d86fd71a65cab9ba642a11253fbb98f53da8
Dusty-Bship <dustyjblankenship@students.abtech.edu>
2023-10-18 12:58:39 -0400

    init commit asgn08

--- asgn07-oop-db-intro/chain_gang/private/db_credentials.php
+++ asgn07-oop-db-intro/chain_gang/private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
 define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "");
-define("DB_NAME", "chain_gang");
+define("DB_USER", "uln2ilnl9mmqh");
+define("DB_PASS", "Potatocakes123");
+define("DB_NAME", "dbluvjr2lglcki");
 ?>
