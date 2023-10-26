8dd61d0216651ca319577519f5927bf60349cbba
Dusty-Bship <dustyjblankenship@students.abtech.edu>
2023-10-26 14:08:27 -0400

    fixed git repo

--- asgn08-active-birds/private/db_credentials.php
+++ asgn08-active-birds/private/db_credentials.php
@@ -6,9 +6,8 @@
 // 3. Unique credentials if working with multiple developers
 
 define("DB_SERVER", "localhost");
-define("DB_USER", "sabirdsUser");
-define("DB_PASS", "cassowary");
-define("DB_NAME", "sabirds");
-
+define("DB_USER", "");
+define("DB_PASS", "");
+define("DB_NAME", "");
 
 ?>
